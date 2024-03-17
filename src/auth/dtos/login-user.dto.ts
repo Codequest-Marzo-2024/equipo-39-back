af45ef9c6bdc8a51f778a5feb38db63a2686874e
Joel M. Boada <jmboada30@gmail.com>
2024-03-17 16:35:32 -0400

    Add Swagger annotations to Auth and Users controllers

--- src/auth/dtos/login-user.dto.ts
+++ src/auth/dtos/login-user.dto.ts
@@ -5,11 +5,21 @@ import {
   MaxLength,
   MinLength,
 } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class LoginUserDto {
+  @ApiProperty({
+    description: 'Email should be a valid email address',
+  })
   @IsEmail()
   email: string;
 
+  @ApiProperty({
+    minLength: 6,
+    maxLength: 50,
+    description:
+      'Password should contain at least one uppercase letter, one lowercase letter, one number and special character and must be at least 6 characters long',
+  })
   @IsString()
   @MinLength(6)
   @MaxLength(50)
