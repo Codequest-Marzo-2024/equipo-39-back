ca22ee89f04eb23f58d9c9d8112d498fdcefdd91
Joel M. Boada <jmboada30@gmail.com>
2024-03-17 00:18:06 -0400

    Refactor error handling in handlerErrorDB function

--- src/common/utils/handler-errors-db.ts
+++ src/common/utils/handler-errors-db.ts
@@ -1,21 +1,35 @@
 import { BadRequestException } from '@nestjs/common';
-import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import {
+  PrismaClientKnownRequestError,
+  PrismaClientValidationError,
+} from '@prisma/client/runtime/library';
 const handlerErrorDB = (error: unknown, res: string) => {
   let message = '';
+  let target = '';
 
   if (error instanceof PrismaClientKnownRequestError) {
     error.meta?.modelName ? (res = error.meta.modelName as string) : res;
+    error.meta?.target
+      ? (target = (error.meta.target as string[]).join(','))
+      : '';
     if (error.code === 'P2002') {
-      message = `${res} already exists`;
+      message = `${res} ${'(' + target + ')'} already exists`;
     } else if (error.code === 'P2025') {
       message = `${res} not found`;
-    } else if (error.code === 'P2025') {
-      message = `Invalid data for ${res}`;
+    } else if (error.message.includes('Unique constraint failed')) {
+      message = `Unique constraint failed in ${res}`;
     } else {
       message = `Error in ${res}`;
     }
   }
 
+  if (error instanceof PrismaClientValidationError) {
+    if (error.message.includes('Invalid value for argument')) {
+      message = `Invalid value for argument in ${res}`;
+    } else {
+      message = `Error in ${res}`;
+    }
+  }
   throw new BadRequestException(message);
 };
 
