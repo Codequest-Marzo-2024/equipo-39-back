3d09534400602652241cfebf04573b7a112ea5ad
Joel M. Boada <jmboada30@gmail.com>
2024-03-17 00:18:54 -0400

    Refactor raffles module imports and providers

--- src/raffles/raffles.module.ts
+++ src/raffles/raffles.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
-import { RafflesService } from './raffles.service';
-import { RafflesController } from './raffles.controller';
+import { RafflesService } from './services/raffles.service';
+import { RafflesController } from './controllers/raffles.controller';
 import { PrismaService } from 'src/prisma.service';
+import { ParticipantsService } from './services/participants.service';
+import { DiscordApiService } from './services/discord-api.service';
+import { DrawRaffleService } from './services/draw-raffle.service';
 
 @Module({
   controllers: [RafflesController],
-  providers: [PrismaService, RafflesService],
+  providers: [
+    PrismaService,
+    RafflesService,
+    ParticipantsService,
+    DiscordApiService,
+    DrawRaffleService,
+  ],
 })
 export class RafflesModule {}
