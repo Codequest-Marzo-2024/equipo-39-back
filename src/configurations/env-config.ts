34e65c50142e065b70abbaac114e14640339f5f6
Joel M. Boada <jmboada30@gmail.com>
2024-03-16 20:58:44 -0400

    Add environment configuration files and validation schema

---
+++ src/configurations/env-config.ts
@@ -0,0 +1,35 @@
+import { registerAs } from '@nestjs/config';
+
+const envConfig = () => ({
+  app: {
+    host: process.env.APP_HOST,
+    port: process.env.APP_PORT,
+    staticPath: process.env.STATIC_PATH,
+  },
+
+  db: {
+    host: process.env.DB_HOST,
+    port: +process.env.DB_PORT,
+    database: process.env.DB_DATABASE,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+  },
+
+  dbPrisma: {
+    databaseUrl: process.env.DATABASE_URL,
+  },
+
+  pgAdmin: {
+    email: process.env.PGADMIN_EMAIL,
+    password: process.env.PGADMIN_PASSWORD,
+  },
+
+  docker: {
+    portPG: process.env.DOCKER_PORT_PG,
+    volumePG: process.env.DOCKER_VOLUME_PG,
+  },
+
+  jwtToken: process.env.JWT_SECRET,
+});
+
+export default registerAs('config', envConfig);
---
+++ src/configurations/joi-validation.ts
@@ -0,0 +1,36 @@
+import * as Joi from 'joi';
+
+export const JoiValidationSchema = Joi.object({
+  app: Joi.object({
+    APP_HOST: Joi.string().required(),
+    APP_PORT: Joi.number().required(),
+    STATIC_PATH: Joi.string().required(),
+  }),
+
+  db: Joi.object({
+    DB_HOST: Joi.string().required(),
+    DB_PORT: Joi.number().required(),
+    DB_DATABASE: Joi.string().required(),
+    DB_USERNAME: Joi.string().required(),
+    DB_PASSWORD: Joi.string().required(),
+  }),
+
+  dbPrisma: Joi.object({
+    DATABASE_URL: Joi.string().required(),
+  }),
+
+  pgAdmin: Joi.object({
+    PGADMIN_EMAIL: Joi.string().required(),
+    PGADMIN_PASSWORD: Joi.string().required(),
+  }),
+
+  docker: Joi.object({
+    DOCKER_PORT_PG: Joi.number().required(),
+    DOCKER_VOLUME_PG: Joi.string().required(),
+  }),
+
+  JWT_SECRET: Joi.string().required(),
+
+  LOGIN_EMAIL: Joi.string().required(),
+  LOGIN_PASSWORD: Joi.string().required(),
+});
